f79c9c28889b27c08f5f0e921664e3d0091b9415
Matt Scarpino <mattscarpino@Matts-MBP.lan>
2023-04-17 10:29:54 -0400

    attempts to make prices scroll and fix pictures

--- vue/src/components/HomePage.vue
+++ vue/src/components/HomePage.vue
@@ -1,92 +1,94 @@
 <template>
   <div>
-    <h1 id="address">51 Surf Court</h1>
-    <h4>Hilton Head Island, South Carolina</h4>
-    <div id="pics">
-      <img id="main-photo" src="../../assets/HHlivingroom.png" />
-      <img
-        id="living-room"
-        class="other-photos"
-        src="../../assets/HHlivingroomblinds.png"
-      />
+    <h1 id="address">Property</h1>
+    <h4 id="location">Hilton Head Island, South Carolina</h4>
+    <div id="gallery">
+      <div id="pics">
+        <img id="main-photo" src="../../assets/HHlivingroom.png" />
+      </div>
+      <div id="square">
+        <img
+          id="living-room"
+          class="other-photos"
+          src="../../assets/HHlivingroomblinds.png"
+        />
 
-      <img
-        id="master-bed"
-        class="other-photos"
-        src="../../assets/HHmasterbed.png"
-      />
-      <img
-        id="guest-bed"
-        class="other-photos"
-        src="../../assets/HHguestbed.png"
-      />
-      <img
-        id="chairs"
-        class="other-photos"
-        src="../../assets/HHmapchairs.png"
-      />
+        <img
+          id="master-bed"
+          class="other-photos"
+          src="../../assets/HHmasterbed.png"
+        />
+        <img
+          id="guest-bed"
+          class="other-photos"
+          src="../../assets/HHguestbed.png"
+        />
+        <img
+          id="chairs"
+          class="other-photos"
+          src="../../assets/HHmapchairs.png"
+        />
+      </div>
     </div>
-    <div>
-      <section id="row">
-        <section class="factoids">
-          <img
-            src="http://clipart-library.com/newimages/bed-clip-art-20.jpg"
-            class="clipArt"
-          />
-          <h1 class="tips">Where You'll Sleep</h1>
-          <p class="description">2 bedroom - 3 bed - 2.5 bath</p>
-        </section>
-        <section class="factoids">
-          <img
-            src="https://www.iconpacks.net/icons/2/free-location-pin-icon-3090-thumb.png"
-            class="clipArt"
-          />
-          <h1 class="tips">Great Location</h1>
-          <p class="description">
-            Walking distance to beach and Coligny Circle
-          </p>
-        </section>
 
-        <section class="factoids">
-          <img
-            src="https://static.vecteezy.com/system/resources/previews/010/788/318/original/double-wrench-icon-with-hammer-icon-related-to-construction-labor-day-line-icon-style-simple-design-editable-vector.jpg"
-            class="clipArt"
-          />
-          <h1 class="tips">Recently Rennovated</h1>
-          <p class="description">Counters / Floors / Furniture / Appliances</p>
-        </section>
+    <section id="TopInformation">
+      <section class="factoids">
+        <img
+          src="http://clipart-library.com/newimages/bed-clip-art-20.jpg"
+          class="clipArt"
+        />
+        <h1 class="tips">Where You'll Sleep</h1>
+        <p class="description">2 bedroom - 3 bed - 2.5 bath</p>
       </section>
-      <section class="pricing">
-        <button @click="springPrices()" class="btn">Spring</button>
-        <button @click="summerPrices()" class="btn">Summer</button>
-        <button @click="fallPrices()" class="btn">Fall</button>
-        <button @click="winterPrices()" class="btn">Winter</button>
-
-        <p class="season">
-          <em>{{ price.season }}</em>
-        </p>
-        <p v-if="num == 1" class="dates"><em>December 1 - February 28</em></p>
-        <p v-if="num == 2" class="dates"><em>March 1 - May 31</em></p>
-        <p v-if="num == 3" class="dates"><em>June 1 - August 31</em></p>
-        <p v-if="num == 4" class="dates"><em>September 1 - November 30</em></p>
-
-        <p class="cost">
-          <b>${{ price.daily }}</b
-          ><em> / night</em>
-        </p>
-
-        <p class="cost">
-          <b>${{ price.weekly }}</b
-          ><em> / week</em>
-        </p>
-        <p class="cost">
-          <b>${{ price.twoweeks }}</b
-          ><em> / month</em>
-        </p>
-
-        <p class="cost"><em>+ Cleaning Fee: </em><b> $150</b></p>
+      <section class="factoids">
+        <img
+          src="https://www.iconpacks.net/icons/2/free-location-pin-icon-3090-thumb.png"
+          class="clipArt"
+        />
+        <h1 class="tips">Great Location</h1>
+        <p class="description">Walking distance to beach and Coligny Circle</p>
       </section>
-    </div>
+
+      <section class="factoids">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/010/788/318/original/double-wrench-icon-with-hammer-icon-related-to-construction-labor-day-line-icon-style-simple-design-editable-vector.jpg"
+          class="clipArt"
+        />
+        <h1 class="tips">Recently Rennovated</h1>
+        <p class="description">Counters / Floors / Furniture / Appliances</p>
+      </section>
+    </section>
+    <section class="pricing">
+      <button @click="springPrices()" class="btn">Spring</button>
+      <button @click="summerPrices()" class="btn">Summer</button>
+      <button @click="fallPrices()" class="btn">Fall</button>
+      <button @click="winterPrices()" class="btn">Winter</button>
+
+      <p class="season">
+        <em>{{ price.season }}</em>
+      </p>
+      <p v-if="num == 1" class="dates"><em>December 1 - February 28</em></p>
+      <p v-if="num == 2" class="dates"><em>March 1 - May 31</em></p>
+      <p v-if="num == 3" class="dates"><em>June 1 - August 31</em></p>
+      <p v-if="num == 4" class="dates"><em>September 1 - November 30</em></p>
+
+      <p class="cost">
+        <b>${{ price.daily }}</b
+        ><em> / night</em>
+      </p>
+
+      <p class="cost">
+        <b>${{ price.weekly }}</b
+        ><em> / week</em>
+      </p>
+      <p class="cost">
+        <b>${{ price.twoweeks }}</b
+        ><em> / month</em>
+      </p>
+
+      <p class="cost"><em>+ Cleaning Fee: </em><b> $150</b></p>
+    </section>
+
     <div class="bottom">
       <section id="yaa">
         <h1 class="descrp">Description</h1>
@@ -246,7 +248,7 @@ export default {
   width: 10%;
 }
 
-#row {
+#TopInformation {
   display: inline-block;
   margin: 5%;
   padding: 2%;
@@ -257,15 +259,15 @@ export default {
 }
 
 .pricing {
+  /* position: fixed; */
   display: inline-block;
-  justify-content: right;
+  /* justify-content: right; */
   margin-left: 5%;
   width: 35%;
-  height: 10%;
+  /* height: 10%; */
   padding: 2%;
   box-sizing: border-box;
   border-radius: 5%;
-
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px 0px,
     rgba(0, 0, 0, 0.23) 0px 3px 6px 0px;
 }
@@ -310,9 +312,10 @@ export default {
   width: fit-content;
   word-break: break-word;
   border: 0;
+  /* height: 3vh; */
 }
 
-#main-photo {
+/* #main-photo {
   grid-area: main;
   width: 100%;
   height: auto;
@@ -325,6 +328,49 @@ export default {
   width: 97%;
   height: auto;
   max-width: 282px;
+} */
+
+/* #pics {
+  display: grid;
+  grid-template-columns: 4;
+  grid-template-rows: 3;
+  grid-template-areas:
+    "main main living guest"
+    "main main bed chairs";
+  gap: 1%;
+  justify-content: center;
+} */
+#pics {
+  grid-area: main;
+  display: flex;
+}
+
+#main-photo {
+  /* flex-basis: 50%; */
+  max-width: 40%;
+  height: auto;
+  border-top-left-radius: 5%;
+  border-bottom-left-radius: 5%;
+  margin-left: 10%;
+}
+
+/* .other-photos {
+  flex-basis: 25%;
+  max-width: 25%;
+  height: 25%;
+} */
+
+#square {
+  grid-area: gal;
+  display: flex;
+  margin-right: 10%;
+}
+
+.other-photos {
+  flex-basis: 25%;
+  flex-wrap: wrap;
+  max-width: 25%;
+  height: 25%;
 }
 #living-room {
   grid-area: living;
@@ -336,22 +382,25 @@ export default {
 }
 #guest-bed {
   grid-area: guest;
-
-  border-top-right-radius: 5%;
 }
 #master-bed {
+  border-top-right-radius: 5%;
   grid-area: bed;
 }
-
-#pics {
-  display: grid;
-  grid-template-columns: 4;
-  grid-template-rows: 3;
-  grid-template-areas:
-    "main main living guest"
-    "main main bed chairs";
-  gap: 1%;
-  justify-content: center;
+#address {
+  font-size: 4vw;
+  padding-left: 14%;
 }
+#location {
+  font-size: 2vw;
+  text-decoration: underline;
+  padding-left: 14%;
+}
+/* #gallery {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1;
+  grid-template-areas: "main gal";
+} */
 </style>
 
