aeadadd058b01647d81858020da9660cbafb744f
Matt Scarpino <mattscarpino@Matts-MBP.lan>
2023-04-18 12:28:38 -0400

    making website responsive

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,13 +1,13 @@
 BEGIN TRANSACTION;
 
 INSERT INTO prices(season, daily, weekly, twoweeks)
-VALUES ('winter',100, 700, 1);
+VALUES ('winter',100, 800, 1);
 
 INSERT INTO prices(season, daily, weekly, twoweeks)
 VALUES ('spring',175, 1225, 0);
 
 INSERT INTO prices(season, daily, weekly, twoweeks)
-VALUES ('summer',200, 1400, 0);
+VALUES ('summer',215, 1500, 0);
 
 INSERT INTO prices(season, daily, weekly, twoweeks)
 VALUES ('fall',175, 1225, 0);
--- vue/src/components/HomePage.vue
+++ vue/src/components/HomePage.vue
@@ -30,7 +30,7 @@
       </div>
     </div>
 
-    <section id="TopInformation">
+    <div id="TopInformation">
       <section class="factoids">
         <img
           src="http://clipart-library.com/newimages/bed-clip-art-20.jpg"
@@ -56,8 +56,8 @@
         <h1 class="tips">Recently Rennovated</h1>
         <p class="description">Counters / Floors / Furniture / Appliances</p>
       </section>
-    </section>
-    <section class="pricing">
+    </div>
+    <section class="pricing" id="allprices">
       <button @click="springPrices()" class="btn">Spring</button>
       <button @click="summerPrices()" class="btn">Summer</button>
       <button @click="fallPrices()" class="btn">Fall</button>
@@ -90,19 +90,17 @@
       </p>
     </section>
 
-    <div class="bottom">
-      <section id="yaa">
-        <h1 class="descrp">Description</h1>
-        <p class="other-desc-not-indented">
-          A two bedroom town house that has been recently rennovated. Has 3 beds
-          and 2.5 baths along with a full kitchen, washer, and dryer. Overlooks
-          the pool and is in walking distance to the beach and Coligny Circle.
-        </p>
-      </section>
-      <section id="bruh">
-        <h1>Ammenenies offered</h1>
-      </section>
-    </div>
+    <section class="pricing">
+      <h1 class="descrp">Description</h1>
+      <p class="other-desc-not-indented">
+        A two bedroom town house that has been recently rennovated. Has 3 beds
+        and 2.5 baths along with a full kitchen, washer, and dryer. Overlooks
+        the pool and is in walking distance to the beach and Coligny Circle.
+      </p>
+    </section>
+    <section id="amenities">
+      <h1>What we offer</h1>
+    </section>
   </div>
 </template>
 
@@ -212,19 +210,176 @@ export default {
 </script>
 
 <style scoped>
-#yaa {
-  display: inline-block;
-  justify-content: left;
-  width: 45%;
+@media (max-width: 820px) {
+  .other-photos {
+    display: none;
+  }
+  div#square {
+    display: none;
+  }
+
+  #gallery {
+    display: flex;
+  }
+  #main-photo {
+    width: 100%;
+    height: auto;
+  }
 }
-#bruh {
+@media (max-width: 500px) {
+  #TopInformation {
+    display: block;
+    margin: auto;
+    padding-left: 12%;
+    padding-right: 12%;
+    padding-top: 8%;
+    padding-bottom: 8%;
+  }
+  .clipArt {
+    display: inline-block;
+    width: 10%;
+    margin-left: 20%;
+  }
+  .tips {
+    display: inline-block;
+    font-size: 3vw;
+    text-align: left;
+    padding-left: 2%;
+    padding-bottom: 0%;
+  }
+  .description {
+    font-size: 2vw;
+    text-align: center;
+    /* padding-left: 15%; */
+    padding-top: 0%;
+  }
+  #allprices {
+    display: block;
+    margin: auto;
+    width: 50%;
+  }
+  .btn {
+    flex-wrap: wrap;
+    background: #5e5df0;
+    border-radius: 999px;
+    box-shadow: #5e5df0 0 10px 20px -10px;
+    box-sizing: border-box;
+    color: #ffffff;
+    cursor: pointer;
+    font-family: Inter, Helvetica, "Apple Color Emoji", "Segoe UI Emoji",
+      NotoColorEmoji, "Noto Color Emoji", "Segoe UI Symbol", "Android Emoji",
+      EmojiSymbols, -apple-system, system-ui, "Segoe UI", Roboto,
+      "Helvetica Neue", "Noto Sans", sans-serif;
+    font-size: 2.3vw;
+    font-weight: 700;
+    line-height: 3vh;
+    opacity: 1;
+    outline: 0 solid transparent;
+    padding: 0.3vh 4%;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    width: fit-content;
+    word-break: break-word;
+    border: 0;
+    margin-left: 1%;
+  }
+  .cost {
+    font-size: 3.3vw;
+    margin-left: 5%;
+  }
+  .dates {
+    font-size: 2.6vw;
+    text-align: center;
+    padding: 0%;
+    margin: 0%;
+  }
+  .season {
+    text-transform: capitalize;
+    font-size: 4.6vw;
+    text-align: center;
+    margin-bottom: 2%;
+    margin-top: 5%;
+  }
+}
+
+@media (min-width: 501px) {
+  #TopInformation {
+    display: inline-block;
+    margin: 5%;
+    padding: 2%;
+    width: 40%;
+  }
+  .description {
+    font-size: 1.7vw;
+    text-align: left;
+    padding-left: 15%;
+    padding-top: 0%;
+  }
+
+  .tips {
+    display: inline-block;
+    font-size: 2.7vw;
+    text-align: left;
+    padding-left: 2%;
+    padding-bottom: 0%;
+  }
+
+  .clipArt {
+    display: inline-block;
+    width: 10%;
+  }
+  .btn {
+    flex-wrap: wrap;
+    background: #5e5df0;
+    border-radius: 999px;
+    box-shadow: #5e5df0 0 10px 20px -10px;
+    box-sizing: border-box;
+    color: #ffffff;
+    cursor: pointer;
+    font-family: Inter, Helvetica, "Apple Color Emoji", "Segoe UI Emoji",
+      NotoColorEmoji, "Noto Color Emoji", "Segoe UI Symbol", "Android Emoji",
+      EmojiSymbols, -apple-system, system-ui, "Segoe UI", Roboto,
+      "Helvetica Neue", "Noto Sans", sans-serif;
+    font-size: 1.7vw;
+    font-weight: 700;
+    line-height: 4vh;
+    opacity: 1;
+    outline: 0 solid transparent;
+    padding: 0.3vh 4%;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    width: fit-content;
+    word-break: break-word;
+    border: 0;
+  }
+  .cost {
+    font-size: 2.7vw;
+  }
+  .dates {
+    font-size: 2vw;
+    text-align: center;
+    padding: 0%;
+    margin: 0%;
+  }
+  .season {
+    text-transform: capitalize;
+    font-size: 4vw;
+    text-align: center;
+    margin-bottom: 2%;
+    margin-top: 5%;
+  }
+}
+
+#amenities {
   display: inline-block;
   justify-content: right;
   margin-left: 20%;
   width: 35%;
 }
 
-#other-desc-not-indented {
+.other-desc-not-indented {
   font-size: 1.7vw;
   text-align: center;
 }
@@ -234,152 +389,61 @@ export default {
   text-align: center;
 }
 
-.description {
-  font-size: 1.7vw;
-  text-align: left;
-  padding-left: 15%;
-  padding-top: 0%;
-}
-
-.tips {
-  display: inline-block;
-  font-size: 2.7vw;
-  text-align: left;
-  padding-left: 2%;
-  padding-bottom: 0%;
-}
-
-.clipArt {
-  display: inline-block;
-  width: 10%;
-}
-
-#TopInformation {
-  display: inline-block;
-  margin: 5%;
-  padding: 2%;
-  width: 40%;
-}
 .factoids {
   border-bottom: 0.1em solid #5e5df0;
 }
 
 .pricing {
-  /* position: fixed; */
   display: inline-block;
-  /* justify-content: right; */
-  margin-left: 5%;
   width: 35%;
-  /* height: 10%; */
+  background-color: #f0f0f0;
   padding: 2%;
+  margin: 5%;
+  align-content: center;
   box-sizing: border-box;
   border-radius: 5%;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px 0px,
     rgba(0, 0, 0, 0.23) 0px 3px 6px 0px;
 }
-.cost {
-  font-size: 2.7vw;
-}
-.dates {
-  font-size: 2vw;
-  text-align: center;
-  padding: 0%;
-  margin: 0%;
-}
-.season {
-  font-size: 4vw;
-  text-align: center;
-  margin-bottom: 2%;
-  margin-top: 5%;
-}
 
 /* CSS */
-.btn {
-  flex-wrap: wrap;
-  background: #5e5df0;
-  border-radius: 999px;
-  box-shadow: #5e5df0 0 10px 20px -10px;
-  box-sizing: border-box;
-  color: #ffffff;
-  cursor: pointer;
-  font-family: Inter, Helvetica, "Apple Color Emoji", "Segoe UI Emoji",
-    NotoColorEmoji, "Noto Color Emoji", "Segoe UI Symbol", "Android Emoji",
-    EmojiSymbols, -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue",
-    "Noto Sans", sans-serif;
-  font-size: 1.7vw;
-  font-weight: 700;
-  line-height: 5vh;
-  opacity: 1;
-  outline: 0 solid transparent;
-  padding: 0.3vh 4%;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  width: fit-content;
-  word-break: break-word;
-  border: 0;
-  /* height: 3vh; */
+
+@media (min-width: 821px) {
+  #gallery {
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: 10%;
+    margin-right: 10%;
+    position: relative;
+  }
+  #main-photo {
+    flex: 1 1 40%; /* Set flex properties to make it take up 100% of the container width */
+    height: auto;
+    border-bottom-left-radius: 3%;
+    border-top-left-radius: 3%;
+    margin-right: 10px;
+  }
+
+  #square {
+    flex: 1 1 40%; /* Set flex properties to make it take up 50% of the container width */
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+  }
+  #square img {
+    width: 100%;
+    height: auto;
+  }
+
+  #chairs {
+    border-bottom-right-radius: 5%;
+  }
+
+  #master-bed {
+    border-top-right-radius: 5%;
+  }
 }
 
-/* #main-photo {
-  grid-area: main;
-  width: 100%;
-  height: auto;
-  border-top-left-radius: 5%;
-  border-bottom-left-radius: 5%;
-}
-
-.other-photos {
-  grid-area: other;
-  width: 97%;
-  height: auto;
-  max-width: 282px;
-} */
-
-/* #pics {
-  display: grid;
-  grid-template-columns: 4;
-  grid-template-rows: 3;
-  grid-template-areas:
-    "main main living guest"
-    "main main bed chairs";
-  gap: 1%;
-  justify-content: center;
-} */
-#gallery {
-  /* grid-area: main; */
-  display: flex;
-  flex-wrap: wrap;
-  margin-left: 10%;
-  margin-right: 10%;
-}
-
-#main-photo {
-  flex: 1 1 40%; /* Set flex properties to make it take up 100% of the container width */
-  height: auto;
-  border-bottom-left-radius: 3%;
-  border-top-left-radius: 3%;
-  margin-right: 10px;
-}
-
-#square {
-  flex: 1 1 40%; /* Set flex properties to make it take up 50% of the container width */
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 10px;
-}
-#square img {
-  width: 100%;
-  height: auto;
-}
-
-#chairs {
-  border-bottom-right-radius: 5%;
-}
-
-#master-bed {
-  border-top-right-radius: 5%;
-}
 #address {
   font-size: 4vw;
   padding-left: 14%;
